aacbda565457cda69a2cff66fa59102779781570
xvntr <164462454+xvntr@users.noreply.github.com>
2024-04-06 04:55:23 +0700

    validation, usercontroller, error alert on form

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExampleController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [ExampleController::class, "homepage"]);
 Route::get('/about', [ExampleController::class, "aboutPage"]);
+
+Route::post('/register', [UserController::class, 'register']);
