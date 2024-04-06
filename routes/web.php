6a1af922056cf37a8485c2929e0e4dbbe3d2a7b7
xvntr <164462454+xvntr@users.noreply.github.com>
2024-04-06 12:46:02 +0700

    add post model and input request

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
+// User Related Routes
 Route::get('/', [UserController::class, "showCorrecthomepage"]);
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+// Blog Related Routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
